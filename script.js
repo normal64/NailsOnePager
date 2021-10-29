5c41b31154866ad13e4cf5a5872d593b3e4f9e07
Denis Poplavskii <normal64@mail.ru>
2021-10-29 21:13:40 +0200

    simpleDimple

---
+++ script.js
@@ -0,0 +1,14 @@
+//open more nail samples
+const extendShowCaseBtn = document.getElementById("extendShowCaseBtn");
+extendShowCaseBtn.addEventListener("click", theExtend);
+console.log("chuj");
+
+function theExtend(){
+  
+    let images = document.getElementsByClassName("bWrapper")[0].getElementsByTagName("img");
+    console.log(images);
+    let imagesArr = [...images]
+    imagesArr.map(elem =>  elem.classList.remove("hidden"))
+    console.log(images);
+
+}   
